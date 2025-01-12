d72a3e9b6865710ae5d1cfdb9738cedd53c7887c
Krishna Thakkar <work.krishnat@gmail.com>
2025-01-12 11:15:29 +0530

    update webhook

--- src/app/api/clerk/accept-organisation/route.ts
+++ src/app/api/clerk/accept-organisation/route.ts
@@ -1,42 +1,71 @@
 import prisma from "@/lib/db";
-import { auth } from "@clerk/nextjs/server";
 
 export async function POST(req: Request) {
   try {
-    const { userId } = await auth();
     const { data, type } = await req.json();
 
-    const organisation = await prisma.organisation.findUnique({
-      where: {
-        clerkOrgId: data.organization_id,
-      },
-    });
+    if (type == "organizationMembership.created") {
+      const organisation = await prisma.organisation.findUnique({
+        where: {
+          clerkOrgId: data.organization.id,
+        },
+      });
 
-    if (!organisation) {
-      return new Response("Organization not found", { status: 404 });
+      if (!organisation) {
+        return new Response("Organization not found", { status: 404 });
+      }
+
+      const user = await prisma.user.findUnique({
+        where: {
+          clerkUserId: data.public_user_data.user_id,
+        },
+      });
+
+      if (!user) {
+        return new Response("User not found", { status: 404 });
+      }
+
+      await prisma.organisationMembership.create({
+        data: {
+          organisationId: organisation.id,
+          userId: user.id,
+          role: data.role,
+        },
+      });
+
+      return new Response("Organization membership created", { status: 200 });
+    } else if (type == "organizationMembership.deleted") {
+      const organisation = await prisma.organisation.findUnique({
+        where: {
+          clerkOrgId: data.organization.id,
+        },
+      });
+
+      if (!organisation) {
+        return new Response("Organization not found", { status: 404 });
+      }
+
+      const user = await prisma.user.findUnique({
+        where: {
+          clerkUserId: data.public_user_data.user_id,
+        },
+      });
+
+      if (!user) {
+        return new Response("User not found", { status: 404 });
+      }
+
+      await prisma.organisationMembership.deleteMany({
+        where: {
+          organisationId: organisation.id,
+          userId: user.id,
+        },
+      });
+
+      return new Response("Organization membership deleted", { status: 200 });
     }
-
-    const user = await prisma.user.findUnique({
-      where: {
-        clerkUserId: userId!,
-      },
-    });
-
-    if (!user) {
-      return new Response("User not found", { status: 404 });
-    }
-
-    await prisma.organisationMembership.create({
-      data: {
-        organisationId: organisation.id,
-        userId: user.id,
-        role: data.role,
-      },
-    });
-
-    return new Response("Organization membership created", { status: 200 });
   } catch (error) {
-    console.error(error);
+    console.log(error);
     return new Response("Error webhook", { status: 400 });
   }
 }
